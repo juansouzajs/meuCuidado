0aa692e5b30aae7b9f7b4fe16076258e4db8bcb0
Windows <juanlucas.paiva@hotmail.com>
2026-01-24 20:59:21 -0300

    layout de views e administrador

---
+++ meuCuidado.Dominio/ViewModels/DocumentoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace meuCuidado.Dominio.ViewModels
+{
+    public class DocumentoViewModel
+    {
+        public string Nome { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ meuCuidado.Dominio/ViewModels/SolicitacaoProfissionalViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace meuCuidado.Dominio.ViewModels
+{
+    public class SolicitacaoProfissionalViewModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string TipoProfissional { get; set; }
+
+        public DateTime DataSolicitacao { get; set; }
+    }
+}
